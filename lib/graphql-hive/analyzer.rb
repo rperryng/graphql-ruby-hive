5bc60f8dad77e2581cf2c2a807c176675e8c1bd1
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2023-02-15 16:45:18 -0500

    support input object fields (#13)
    * fix: skip parent for input objects
    
    * add more input object test cases
    
    * test case for explicit enum value in input object
    
    * re-do argument extraction
    
    * hooray
    
    * refactor
    
    * rubocop
    
    * handle list values
    
    * rubocop
    
    * remove pry statements
    
    * trigger new build

--- lib/graphql-hive/analyzer.rb
+++ lib/graphql-hive/analyzer.rb
@@ -9,23 +9,26 @@ module GraphQL
         @used_fields = Set.new
       end
 
-      def on_leave_field(node, _parent, visitor)
+      def on_enter_field(node, _parent, visitor)
         @used_fields.add(visitor.parent_type_definition.graphql_name)
-        @used_fields.add([visitor.parent_type_definition.graphql_name, node.name].join('.'))
+        @used_fields.add(make_id(visitor.parent_type_definition.graphql_name, node.name))
       end
 
-      def on_leave_argument(node, parent, visitor)
-        @used_fields.add([visitor.parent_type_definition.graphql_name, parent.name, node.name].join('.'))
-
+      # Visitor also calls 'on_enter_argument' when visiting input object fields in arguments
+      def on_enter_argument(node, parent, visitor)
         arg_type = visitor.argument_definition.type.unwrap
-        arg_type_kind = visitor.argument_definition.type.unwrap.kind
-        if arg_type_kind.input_object?
-          @used_fields.add(arg_type.graphql_name)
-          arg_type.arguments.each do |arg|
-            @used_fields.add([arg_type.graphql_name, arg[0]].join('.'))
-          end
-        elsif arg_type_kind.enum?
-          @used_fields.add([arg_type.graphql_name, node.value.name].join('.'))
+        @used_fields.add(arg_type.graphql_name)
+
+        # collect field argument path
+        # input object fields won't have a "parent.name" method available
+        if parent.respond_to?(:name)
+          @used_fields.add(make_id(visitor.parent_type_definition.graphql_name, parent.name, node.name))
+        end
+
+        if arg_type.kind.input_object?
+          collect_input_object_fields(node, arg_type)
+        elsif arg_type.kind.enum?
+          collect_enum_values(node, arg_type)
         end
       end
 
@@ -33,6 +36,44 @@ module GraphQL
 
       def result
         @used_fields
+      end
+
+      private
+
+      def collect_input_object_fields(node, input_type)
+        case node.value
+        when GraphQL::Language::Nodes::VariableIdentifier
+          input_type.all_argument_definitions.map(&:graphql_name).each do |n|
+            @used_fields.add(make_id(input_type.graphql_name, n))
+          end
+        when Array
+          node.value.flat_map(&:arguments).map(&:name).each do |n|
+            @used_fields.add(make_id(input_type.graphql_name, n))
+          end
+        else
+          node.value.arguments.map(&:name).each do |n|
+            @used_fields.add(make_id(input_type.graphql_name, n))
+          end
+        end
+      end
+
+      def collect_enum_values(node, enum_type)
+        case node.value
+        when GraphQL::Language::Nodes::VariableIdentifier
+          enum_type.values.values.map(&:graphql_name).each do |n|
+            @used_fields.add(make_id(enum_type.graphql_name, n))
+          end
+        when Array
+          node.value.map(&:name).each do |n|
+            @used_fields.add(make_id(enum_type.graphql_name, n))
+          end
+        else
+          @used_fields.add(make_id(enum_type.graphql_name, node.value.name))
+        end
+      end
+
+      def make_id(*tokens)
+        tokens.join('.')
       end
     end
   end
--- spec/graphql/graphql-hive/analyzer_spec.rb
+++ spec/graphql/graphql-hive/analyzer_spec.rb
@@ -3,56 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe 'GraphQL::Hive::Analyzer' do
-  let(:schema) do
-    GraphQL::Schema.from_definition(%|
-    """
-    A blog post
-    """
-    type Post {
-      id: ID!
-      title: String!
-      truncatedPreview: String!
-    }
-
-    """
-    Query Post arguments
-    """
-    input PostInput {
-      id: ID!
-    }
-
-    """
-    The query root of this schema
-    """
-    type Query {
-      """
-      Find a post by ID
-      """
-      post(input: [PostInput!]!, test: TestEnum!): Post
-    }
-
-    enum TestEnum {
-      TEST1
-      TEST2
-      TEST3
-    }|)
-  end
-
-  let(:query_string) do
-    %|
-      query GetPost2($input: [PostInput!]!) {
-        post(input: $input, test: TEST1) {
-          title
-
-          myId: id
-        }
-      }
-    |
-  end
-
-  it 'should return all used fields, input type and enum values' do
+  subject(:used_fields) do
     query = GraphQL::Query.new(schema, query_string)
-
     analyzer = GraphQL::Hive::Analyzer.new(query)
     visitor = GraphQL::Analysis::AST::Visitor.new(
       query: query,
@@ -60,8 +12,341 @@ RSpec.describe 'GraphQL::Hive::Analyzer' do
     )
 
     visitor.visit
+    analyzer.used_fields.to_a
+  end
 
-    expect(analyzer.used_fields.to_a).to eq(['Query.post.input', 'PostInput', 'PostInput.id', 'Query.post.test',
-                                             'TestEnum.TEST1', 'Post', 'Post.title', 'Post.id', 'Query', 'Query.post'])
+  let(:schema) do
+    GraphQL::Schema.from_definition(%|
+      type Query {
+        project(selector: ProjectSelectorInput!): Project
+        projectsByType(type: ProjectType!): [Project!]!
+        projectsByManyTypes(type: [ProjectType!]!): [Project!]!
+        projects(filter: FilterInput): [Project!]!
+      }
+
+      type Mutation {
+        deleteProject(selector: ProjectSelectorInput!): DeleteProjectPayload!
+      }
+
+      input ProjectSelectorInput {
+        organization: ID!
+        project: ID!
+      }
+
+      input FilterInput {
+        type: ProjectType
+        pagination: PaginationInput
+        order: [ProjectOrderByInput!]
+      }
+
+      input PaginationInput {
+        limit: Int
+        offset: Int
+      }
+
+      input ProjectOrderByInput {
+        field: String!
+        direction: OrderDirection
+      }
+
+      enum OrderDirection {
+        ASC
+        DESC
+      }
+
+      type ProjectSelector {
+        organization: ID!
+        project: ID!
+      }
+
+      type DeleteProjectPayload {
+        selector: ProjectSelector!
+        deletedProject: Project!
+      }
+
+      type Project {
+        id: ID!
+        cleanId: ID!
+        name: String!
+        type: ProjectType!
+        buildUrl: String
+        validationUrl: String
+      }
+
+      enum ProjectType {
+        FEDERATION
+        STITCHING
+        SINGLE
+        CUSTOM
+      }
+    |)
+  end
+
+  let(:query_string) do
+    %|
+      mutation deleteProject($selector: ProjectSelectorInput!) {
+        deleteProject(selector: $selector) {
+          selector {
+            organization
+            project
+          }
+
+          deletedProject {
+            id
+            cleanId
+            name
+            type
+          }
+        }
+      }
+    |
+  end
+
+  it 'collects used fields and arguments' do
+    expect(used_fields).to include(
+      'DeleteProjectPayload',
+      'DeleteProjectPayload.selector',
+      'Mutation',
+      'Mutation.deleteProject',
+      'Mutation.deleteProject.selector',
+      'Project',
+      'Project.cleanId',
+      'Project.id',
+      'Project.name',
+      'Project.type',
+      'ProjectSelector',
+      'ProjectSelector.organization',
+      'ProjectSelector.project'
+    )
+    expect(used_fields).not_to include(
+      'Project.buildUrl',
+      'Project.validationUrl'
+    )
+  end
+
+  it 'collects used input object fields' do
+    expect(used_fields).to include(
+      'ProjectSelectorInput',
+      'ProjectSelectorInput.organization',
+      'ProjectSelectorInput.project'
+    )
+  end
+
+  context 'with enum value in argument' do
+    let(:query_string) do
+      %|
+      query getProjects($type: ProjectType!) {
+        projectsByType(type: $type) {
+          id
+        }
+      }
+    |
+    end
+
+    it 'collects all values of that enum type' do
+      expect(used_fields).to include(
+        'ProjectType.FEDERATION',
+        'ProjectType.STITCHING',
+        'ProjectType.SINGLE',
+        'ProjectType.CUSTOM'
+      )
+    end
+  end
+
+  context 'with enum value hardcoded into the query' do
+    let(:query_string) do
+      %|
+      query getProjects {
+        projectsByType(type: FEDERATION) {
+          id
+        }
+      }
+    |
+    end
+
+    it 'collects used enumn values' do
+      expect(used_fields).to include(
+        'ProjectType.FEDERATION'
+      )
+      expect(used_fields).not_to include(
+        'ProjectType.STITCHING',
+        'ProjectType.SINGLE',
+        'ProjectType.CUSTOM'
+      )
+    end
+  end
+
+  context 'with only some input object fields used' do
+    let(:query_string) do
+      %|
+      query getProjects($limit: Int!, $type: ProjectType!) {
+        projects(filter: { pagination: { limit: $limit }, type: $type }) {
+          id
+        }
+      }
+    |
+    end
+
+    it 'collects fields of specified input objects' do
+      expect(used_fields).to include(
+        'FilterInput',
+        'FilterInput.pagination',
+        'FilterInput.type',
+        'PaginationInput',
+        'PaginationInput.limit',
+        'Query.projects.filter'
+      )
+
+      expect(used_fields).not_to include(
+        'FilterInput.order',
+        'PaginationInput.offset'
+      )
+    end
+  end
+
+  context 'with query containining both input object variable as well as explicit input object fields' do
+    let(:query_string) do
+      %|
+      query getProjects($type: ProjectType!, $pagination: PaginationInput) {
+        projects(filter: { pagination: $pagination, type: $type }) {
+          id
+        }
+      }
+    |
+    end
+
+    it 'collects all fields of input object variable, but only specific fields for the other input object type' do
+      expect(used_fields).to include(
+        'PaginationInput',
+        'PaginationInput.limit',
+        'PaginationInput.offset',
+        'FilterInput.type',
+        'FilterInput.pagination'
+      )
+
+      expect(used_fields).not_to include(
+        'FilterInput.order'
+      )
+    end
+  end
+
+  context 'with query containing list of input objects' do
+    let(:query_string) do
+      %|
+      query getProjects($limit: Int!, $type: ProjectType!) {
+        projects(filter: {
+          pagination: {
+            limit: $limit
+          },
+          type: $type,
+          order: [
+            {
+              field: "id",
+            }
+          ]
+        }) {
+          id
+        }
+      }
+      |
+    end
+
+    it 'collects fields from the input values' do
+      expect(used_fields).to include(
+        'ProjectOrderByInput.field'
+      )
+      expect(used_fields).to_not include(
+        'ProjectOrderByInput.direction'
+      )
+    end
+  end
+
+  context 'with query containing enum value inside input object list' do
+    let(:query_string) do
+      %|
+      query getProjects($limit: Int!, $type: ProjectType!) {
+        projects(filter: {
+          pagination: {
+            limit: $limit
+          },
+          type: $type,
+          order: [
+            {
+              field: "id",
+              direction: ASC
+            },
+            {
+              field: "cleanId",
+            }
+          ]
+        }) {
+          id
+        }
+      }
+      |
+    end
+
+    it 'collects the used enum values' do
+      expect(used_fields).to include(
+        'OrderDirection.ASC'
+      )
+
+      expect(used_fields).not_to include(
+        'OrderDirection.DESC'
+      )
+    end
+  end
+
+  context 'with query containing enum value in nested input object list' do
+    let(:query_string) do
+      %|
+      query getProjects($type: ProjectType!, $limit: Int) {
+        projects(filter: {
+          pagination: {
+            limit: $limit
+          },
+          type: $type,
+          order: {
+            field: "test",
+            direction: ASC,
+          }
+        }) {
+          id
+        }
+      }
+    |
+    end
+
+    it 'collects the enum value' do
+      expect(used_fields).to include(
+        'OrderDirection.ASC'
+      )
+
+      expect(used_fields).not_to include(
+        'OrderDirection.DESC'
+      )
+    end
+  end
+
+  context 'with query containing argument of enum values' do
+    let(:query_string) do
+      %|
+      query getGatewayProjects {
+        projectsByManyTypes(type: [STITCHING, FEDERATION])
+      }
+      |
+    end
+
+    it 'collects the used enum values' do
+      expect(used_fields).to include(
+        'ProjectType.STITCHING',
+        'ProjectType.FEDERATION'
+      )
+
+      expect(used_fields).not_to include(
+        'ProjectType.SINGLE',
+        'ProjectType.CUSTOM'
+      )
+    end
   end
 end
