85aa5403d5839bd3d8849b550499efb626d3d70d
aryascripts <abhimani@wealthsimple.com>
2024-07-08 14:30:47 -0400

    fix: updated with cops and print statements

--- lib/graphql-hive/printer.rb
+++ lib/graphql-hive/printer.rb
@@ -18,20 +18,17 @@ module GraphQL
       end
 
       # from GraphQL::Language::Printer with sort_by name
-      def print_field(field, indent: "")
+      # ignores aliases
+      def print_field(field, indent: '')
         print_string(indent)
-        if field.alias
-          print_string(field.alias)
-          print_string(": ")
-        end
         print_string(field.name)
         if field.arguments.any?
-          print_string("(")
+          print_string('(')
           field.arguments.sort_by(&:name).each_with_index do |a, i|
             print_argument(a)
-            print_string(", ") if i < field.arguments.size - 1
+            print_string(', ') if i < field.arguments.size - 1
           end
-          print_string(")")
+          print_string(')')
         end
         print_directives(field.directives)
         print_selections(field.selections, indent: indent)
@@ -42,49 +39,47 @@ module GraphQL
       end
 
       # from GraphQL::Language::Printer with sort_by name
-      def print_selections(selections, indent: "")
-        return if selections.empty?
+      def print_selections(selections, indent: '')
+        sorted_nodes = selections.sort_by do |s|
+          next s.name if s.respond_to?(:name)
+          next s.type.name if s.respond_to?(:type)
 
-        print_string(" {\n")
-        selections.sort_by.each do |selection|
-          print_node(selection, indent: indent + "  ")
-          print_string("\n")
+          raise "don't know how to sort selection node: #{s.inspect}"
         end
-        print_string(indent)
-        print_string("}")
+        super(sorted_nodes, indent: indent)
       end
 
       # from GraphQL::Language::Printer with sort_by name
       def print_directive(directive)
-        print_string("@")
+        print_string('@')
         print_string(directive.name)
 
         if directive.arguments.any?
-          print_string("(")
+          print_string('(')
           directive.arguments.sort_by(&:name).each_with_index do |a, i|
             print_argument(a)
-            print_string(", ") if i < directive.arguments.size - 1
+            print_string(', ') if i < directive.arguments.size - 1
           end
-          print_string(")")
+          print_string(')')
         end
       end
 
       # from GraphQL::Language::Printer with sort_by name
-      def print_operation_definition(operation_definition, indent: "")
+      def print_operation_definition(operation_definition, indent: '')
         print_string(indent)
         print_string(operation_definition.operation_type)
         if operation_definition.name
-          print_string(" ")
+          print_string(' ')
           print_string(operation_definition.name)
         end
 
         if operation_definition.variables.any?
-          print_string("(")
-          operation_definition.variables..sort_by(&:name).each_with_index do |v, i|
+          print_string('(')
+          operation_definition.variables.sort_by(&:name).each_with_index do |v, i|
             print_variable_definition(v)
-            print_string(", ") if i < operation_definition.variables.size - 1
+            print_string(', ') if i < operation_definition.variables.size - 1
           end
-          print_string(")")
+          print_string(')')
         end
 
         print_directives(operation_definition.directives)
