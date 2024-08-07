dd4ae9e8b84c5dfebe8322f781c3731565b52ef2
Alina Zeng <alina.zeng@alina.zeng9d70>
2024-08-07 11:31:20 -0400

    update unit tests, implementation with instance variables

--- lib/graphql-hive/sampler.rb
+++ lib/graphql-hive/sampler.rb
@@ -1,30 +1,29 @@
 module GraphQL
   class Hive
     class Sampler
-      @sample_rate = 1
-      @sampler = nil
-      @tracked_operations = nil 
-
       def initialize(client_sampler)
         if (client_sampler.is_a?(Proc))
           @sampler = client_sampler
           @tracked_operations = Hash.new
         elsif (client_sampler.is_a?(Integer))
           @sample_rate = client_sampler
+        else
+          @sample_rate = 1
         end
       end
 
       def should_include(operation)
         if (@sampler)
           if (@tracked_operations.has_key?(operation))
-            @sample_rate = @sampler.call(operation)
+            @sample_rate = @sampler.call(operation) # TODO: determine necessary arguments
+            # TODO: implement keyFn
           else
             @tracked_operations[operation] = true
             @sample_rate = 1
           end
         end
 
-        rand(100) <= @sample_rate
+        rand(0.0..1.0) <= @sample_rate
       end
     end
   end
--- spec/graphql/graphql-hive/sampler_spec.rb
+++ spec/graphql/graphql-hive/sampler_spec.rb
@@ -4,19 +4,46 @@ RSpec.describe GraphQL::Hive::Sampler do
   let(:subject) { described_class.instance }
 
   describe '#initialize' do
-    it 'sets the sample rate if provided a sample rate' do
+    it 'sets the sampler and tracked operations hash, if provided a sampler' do
+      mock_sampler = Proc.new { |sample_context| 1 }
+      sampler_instance = described_class.new(mock_sampler)
+      expect(sampler_instance.instance_variable_get(:@sample_rate)).to eq(nil)
+      expect(sampler_instance.instance_variable_get(:@sampler)).to eq(mock_sampler)
+      expect(sampler_instance.instance_variable_get(:@tracked_operations)).to eq({})
+    end
+
+    it 'sets the sample rate, if provided a sample rate' do
       sampler_instance = described_class.new(0)
       expect(sampler_instance.instance_variable_get(:@sample_rate)).to eq(0)
       expect(sampler_instance.instance_variable_get(:@sampler)).to eq(nil)
       expect(sampler_instance.instance_variable_get(:@tracked_operations)).to eq(nil)
     end
 
-    it 'sets the sampler and tracked operations hash if provided a sampler' do
-      mock_sampler = Proc.new { |operation| true }
-      sampler_instance = described_class.new(mock_sampler)
+    it 'sets the sample rate to 1, if no sample rate or sampler is provided' do
+      sampler_instance = described_class.new(nil)
       expect(sampler_instance.instance_variable_get(:@sample_rate)).to eq(1)
-      expect(sampler_instance.instance_variable_get(:@sampler)).to eq(mock_sampler)
-      expect(sampler_instance.instance_variable_get(:@tracked_operations)).to eq({})
+      expect(sampler_instance.instance_variable_get(:@sampler)).to eq(nil)
+      expect(sampler_instance.instance_variable_get(:@tracked_operations)).to eq(nil)
+    end
+  end
+
+  describe '#should_include' do
+    it 'returns true for the first operation and follows the sampler for the remaining operations, if provided a sampler' do
+      mock_sampler = Proc.new { |sample_context| 0 }
+      sampler_instance = described_class.new(mock_sampler)
+      expect(sampler_instance.should_include('operation')).to eq(true)
+      expect(sampler_instance.should_include('operation')).to eq(false)
+    end
+
+    it 'follows the sample rate for all operations, if provided a sample rate' do
+      sampler_instance = described_class.new(0)
+      expect(sampler_instance.should_include('operation')).to eq(false)
+    end
+
+    it 'returns true for all operations, if no sample rate or sampler is provided' do
+      sampler_instance = described_class.new(nil)
+      expect(sampler_instance.should_include('operation')).to eq(true)
+      expect(sampler_instance.should_include('operation')).to eq(true)
     end
   end
 end
