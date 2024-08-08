ff7972adc80b7106a8e4ed9ddf1002c3f713465b
Alina Zeng <alina.zeng@alina.zeng9d70>
2024-08-08 10:58:58 -0400

    clean mock definition

--- spec/graphql/graphql-hive/usage_reporter_spec.rb
+++ spec/graphql/graphql-hive/usage_reporter_spec.rb
@@ -79,80 +79,37 @@ RSpec.describe GraphQL::Hive::UsageReporter do
   end
 
   describe '#process_operation' do
-    context 'successful operation' do
-      let(:options) do
-        { logger: logger, buffer_size: 2 }
-      end
-      let(:queries) do
-        [GraphQL::Query.new(schema, query_string, variables: {})]
-      end
-      let(:results) do
-        [GraphQL::Query::Result.new(query: queries.first, values: { 'data' => { 'test' => 'test' } })]
-      end
+    let(:queries) { [GraphQL::Query.new(schema, query_string, variables: {})]}
+    let(:results) { [GraphQL::Query::Result.new(query: queries.first, values: { 'data' => { 'test' => 'test' } })] }
 
-      before do
-        allow(client).to receive(:send)
-      end
+    before do
+      allow(client).to receive(:send)
+    end
 
-      it 'processes the operations if the buffer is full' do
-        described_class.new(options, client)
-        subject.send(:process_operations, [operation])
-        
-        expect(client).to have_received(:send).with(
-          '/usage',
-          {
-            map: { '8b8412ce86f3ea7accb931b1a5de335d' =>
-              {
-                fields: %w[Query Query.test],
-                operation: "query TestingHive {\n  test\n}",
-                operationName: 'TestingHive'
-              } },
-            operations: [
-              {
-                execution: { duration: 100_000, errors: [], errorsTotal: 0, ok: true },
-                operationMapKey: '8b8412ce86f3ea7accb931b1a5de335d',
-                timestamp: 1_720_705_946_333
-              }
-            ],
-            size: 1
-          },
-          :usage
-        )
-      end
+    it 'processes the operations if the buffer is full' do
+      described_class.new(options, client)
+      subject.send(:process_operations, [operation])
 
-      # context 'with a client sampler for at least once sampling' do
-      #   let(:options) do
-      #     { logger: logger, buffer_size: 2, collect_usage_sampler: Proc.new { |sampling_context| 0 } }
-      #   end
-
-      #   it 'processes the operations if the buffer is full' do
-      #     described_class.new(options, client)
-
-      #     subject.add_operation(operation)
-      #     subject.add_operation(operation)å
-      #     subject.send(:process_operations, [operation])
-      #     expect(client).to have_received(:send).with(
-      #       '/usage',
-      #       {
-      #         map: { '8b8412ce86f3ea7accb931b1a5de335d' =>
-      #           {
-      #             fields: %w[Query Query.test],
-      #             operation: "query TestingHive {\n  test\n}",
-      #             operationName: 'TestingHive'
-      #           } },
-      #         operations: [
-      #           {
-      #             execution: { duration: 100_000, errors: [], errorsTotal: 0, ok: true },
-      #             operationMapKey: '8b8412ce86f3ea7accb931b1a5de335d',
-      #             timestamp: 1_720_705_946_333
-      #           }
-      #         ],
-      #         size: 1
-      #       },
-      #       :usage
-      #     )
-      #   end
-      # end
+      expect(client).to have_received(:send).with(
+        '/usage',
+        {
+          map: { '8b8412ce86f3ea7accb931b1a5de335d' =>
+            {
+              fields: %w[Query Query.test],
+              operation: "query TestingHive {\n  test\n}",
+              operationName: 'TestingHive'
+            } },
+          operations: [
+            {
+              execution: { duration: 100_000, errors: [], errorsTotal: 0, ok: true },
+              operationMapKey: '8b8412ce86f3ea7accb931b1a5de335d',
+              timestamp: 1_720_705_946_333
+            }
+          ],
+          size: 1
+        },
+        :usage
+      )
     end
   end
 end
