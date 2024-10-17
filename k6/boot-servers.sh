4a4e3f0969f616ed70a3ba8ce5e01bfc0e07c5c0
Cassidy Scheffer <hello@cassidy.codes>
2024-10-17 16:01:08 -0400

    fix error in boot

--- k6/boot-servers.sh
+++ k6/boot-servers.sh
@@ -21,12 +21,16 @@ start_puma_server() {
 
 echo "Installing dependencies..."
 yarn install
-cd graphql-api
+cd graphql-api || {
+  echo "Could not find graphql-api" && exit 1
+}
 bundle install
 cd ..
+
 # Start Node.js server
 echo "Starting usage-api server..."
 LOG_LEVEL=$LOG_LEVEL node usage-api.js | log_with_prefix "usage-api" &
+
 # Start first Puma server
 cd graphql-api || {
   echo "Could not find graphql-api" && exit 1
@@ -37,10 +41,16 @@ start_puma_server false 9292 "hive-disabled"
 # Function to handle shutdown
 shutdown_servers() {
   echo "Received shutdown signal. Shutting down servers..."
-  kill $(lsof -t -i:9291)
-  kill $(lsof -t -i:9292)
-  kill $(lsof -t -i:8888)
-  wait
+
+  for port in 9291 9292 8888; do
+    pid=$(lsof -t -i:$port)
+    if [ -n "$pid" ]; then
+      kill "$pid"
+    else
+      echo "No process found on port $port"
+    fi
+  done
+
   echo "Servers shut down gracefully."
   exit 0
 }
