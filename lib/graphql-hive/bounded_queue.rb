81e15528e1ae6690caf29a56529b61e666ea5e0e
Arya Bhimani <abhimani@wealthsimple.com>
2024-10-29 14:08:10 -0400

    fix: comment for SizedQueue

--- lib/graphql-hive/bounded_queue.rb
+++ lib/graphql-hive/bounded_queue.rb
@@ -1,5 +1,8 @@
 module GraphQL
   class Hive < GraphQL::Tracing::PlatformTracing
+    # BoundedQueue is being used so that the queue does not grow indefinitely
+    # We do not use `SizedQueue` because it blocks the thread when the queue is full with a .wait call
+    # This would go against us not impacting the application performance with the usage reporter
     class BoundedQueue < Thread::Queue
       def initialize(bound:, logger:)
         @bound = bound
